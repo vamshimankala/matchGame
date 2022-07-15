b049b47858ffda72b8afbd22f635a40c6535a122
vamshimankala <mankalavamshi99@outlook.com>
2022-07-15 08:00:27 +0000

    matchGame

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, active, onClick} = props
+
+  const onClickTabItem = () => {
+    onClick(tabDetails.tabId)
+  }
+  return (
+    <li className="tab-item">
+      <button
+        className={`tab-button ${active ? 'active' : ''}`}
+        type="button"
+        onClick={onClickTabItem}
+      >
+        {tabDetails.displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
